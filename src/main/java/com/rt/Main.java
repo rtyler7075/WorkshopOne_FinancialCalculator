cd182d9c3e8a8fc5ec10d3e3f8d4a4e168ed7710
RayShawn Tyler <rtyler@appdev.yearup.org>
2023-04-14 14:06:52 -0400

    Second commit

--- src/main/java/com/rt/Main.java
+++ src/main/java/com/rt/Main.java
@@ -2,6 +2,6 @@ package com.rt;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello Universe!");
     }
 }
